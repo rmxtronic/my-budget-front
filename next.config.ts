e0930e9a43eab38cf1a66a0c6f36e7b568263cf7
Ruben Moreno <rmxtronic@gmail.com>
2026-04-23 06:04:09 -0500

    Revert "Fix CSP eval() violation by replacing webpack eval-based source maps"
    This reverts commit 06588abf94e1e12a3c4aad3549e3d835e5352885.

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  webpack: (config, { dev }) => {
-    if (dev) {
-      config.devtool = "cheap-module-source-map";
-    }
-    return config;
-  },
+  /* config options here */
 };
 
 export default nextConfig;
